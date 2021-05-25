53df1c0174e0650f59594896481bda0431d81f14
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-05-25 16:25:31 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
+objects = [];
+status = "";
+img = "";
+function preload(){
+    img = loadImage('ok.jpg');
+}
+
 
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    video.hide();
-
-    canvas =  createCanvas(480, 380);
-    canvas.center();
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  
 }
 
-function start() {
-    loading = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting DOGS"; 
-}
-
-function modelLoaded()
+function start()
 {
-    console.log("MOAJU LOADESED")
-    status = true;
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting DOgs";
 }
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
 
 function draw() {
-    image(video, 0, 0, 480, 380);
+  image(video, 0, 0, 480, 380);
+      if(status != "")
+      {
+        objectDetector.detect(img, gotResult);
+        for (i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Dogs Detected";
+          document.getElementById("number_of_objects").innerHTML = "Number of Dogs detected are : "+ objects.length;
+ 
+          fill("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
 }
